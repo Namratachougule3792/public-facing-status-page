3e1ce53f1fd2f11164f05275d8eaa1ef211ebbb9
namrata-chougule <namrata.chougle@kiinara.com>
2026-05-28 10:20:05 +0530

    fixed conflict

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,30 +1,25 @@
 export default defineNuxtConfig({
   devtools: { enabled: false },
+
+  ssr: false,
+
   css: ['~/assets/css/main.css'],
-<<<<<<< HEAD
-=======
+
   runtimeConfig: {
     public: {
-      supabaseUrl: process.env.SUPABASE_URL || '',
-      supabaseKey: process.env.SUPABASE_ANON_KEY || ''
+      supabaseUrl:
+        process.env.SUPABASE_URL ||
+        'https://xadzdobviwceujmpxlkx.supabase.co',
+
+      supabaseAnonKey:
+        process.env.SUPABASE_ANON_KEY || ''
     }
   },
->>>>>>> 92f235f (changes added)
+
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {}
     }
-  },
-<<<<<<< HEAD
-  runtimeConfig: {
-    public: {
-      supabaseUrl: process.env.SUPABASE_URL || 'https://xadzdobviwceujmpxlkx.supabase.co',
-      supabaseAnonKey: process.env.SUPABASE_ANON_KEY || ''
-    }
   }
 })
-=======
-  ssr: false
-})
->>>>>>> 92f235f (changes added)
